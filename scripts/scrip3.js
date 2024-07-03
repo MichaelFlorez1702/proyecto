77102c02b65d5dbe4096972cdf3db4bdb73ce0a4
jhonfredybetancur <fredybencar22@gmail.com>
2024-07-03 16:58:41 -0500

    ...

--- scripts/scrip3.js
+++ scripts/scrip3.js
@@ -209,23 +209,4 @@ const products = [
     console.log(buscarProductosPorNombre(products, "Earrings"));
 
 // Ejercicio 4
-    // Función para ordenar un array de productos por precio (ascendente o descendente)
-    function ordenarProductosPorPrecio(productos, orden) {
-        const compararPorPrecio = (a, b) => {
-        if (a.precio < b.precio) {
-            return orden === "ascendente" ? -1 : 1;
-        } else if (a.precio > b.precio) {
-            return orden === "ascendente" ? 1 : -1;
-        } else {
-            return 0;
-        }
-        };
     
-        return productos.sort(compararPorPrecio);
-  }
-  
-  console.log("Productos ordenados ascendentemente por precio:");
-  console.log(ordenarProductosPorPrecio(products, "ascendente"));
-  
-  console.log("\nProductos ordenados descendentemente por precio:");
-  console.log(ordenarProductosPorPrecio(products, "descendente"));
