a68ff7b9f47d7f1ef1b1debb5dbd8a2fd44b60c1
Michael Florez <maicol_1702@hotmail.com>
2024-07-03 16:03:14 -0500

    Depurando

--- scripts/scrip3.js
+++ scripts/scrip3.js
@@ -168,30 +168,22 @@ const products = [
     producto2_line4
 ];
 //Ejercicio 2
-// Función para filtrar productos por tipo
-function filtrarProductosPorTipo(productos, tipoProducto) {
-    const productosFiltrados = productos.filter(producto => producto.tipo === tipoProducto);
-    return productosFiltrados;
-  }
-  
-  // Lista de productos
-  const productos = [
-    { nombre: "Luxury Gems Necklase", tipo: "collar", precio: 168.76 },
-    { nombre: "Reflections Necklace", tipo: "collar", precio: 620.73 },
-    { nombre: "Timeless Halo Earrings", tipo: "aretes", precio: 620.73 },
-    { nombre: "Exquisite Earrings", tipo: "aretes", precio: 327.71 },
+    // Función para filtrar productos por tipo
+    function filtrarProductosPorTipo(productos, tipoProducto) {
+        const productosFiltrados = productos.filter(producto => producto.tipo === tipoProducto);
+        return productosFiltrados;
+    }
     
-  ];
-  
-  // Filtrar productos por tipo de accesorio
-  const accesoriosFiltrados = filtrarProductosPorTipo(productos, "Accesorio");
-  
-  // Mostrar resultado en la consola del navegador
-  console.log("Accesorios disponibles:");
-  console.table(accesoriosFiltrados); 
+    
+    // Filtrar productos por tipo de accesorio
+    const accesoriosFiltrados = filtrarProductosPorTipo(products, "Accesorio");
+    
+    // Mostrar resultado en la consola del navegador
+    console.log("Accesorios disponibles:");
+    console.table(accesoriosFiltrados); 
   
 
-  //Ejercicio 3
+//Ejercicio 3
     function buscarProductosPorNombre(productos, terminoBusqueda) {
         // Convertimos el término de búsqueda a minúsculas para hacer la búsqueda case-insensitive
         const termino = terminoBusqueda.toLowerCase();
@@ -216,32 +208,24 @@ function filtrarProductosPorTipo(productos, tipoProducto) {
     console.log("\nBúsqueda de 'Earrings':");
     console.log(buscarProductosPorNombre(products, "Earrings"));
 
-  // Ejercicio 4
-// Función para ordenar un array de productos por precio (ascendente o descendente)
-function ordenarProductosPorPrecio(productos, orden) {
-    const compararPorPrecio = (a, b) => {
-      if (a.precio < b.precio) {
-        return orden === "ascendente" ? -1 : 1;
-      } else if (a.precio > b.precio) {
-        return orden === "ascendente" ? 1 : -1;
-      } else {
-        return 0;
-      }
-    };
-  
-    return productos.sort(compararPorPrecio);
+// Ejercicio 4
+    // Función para ordenar un array de productos por precio (ascendente o descendente)
+    function ordenarProductosPorPrecio(productos, orden) {
+        const compararPorPrecio = (a, b) => {
+        if (a.precio < b.precio) {
+            return orden === "ascendente" ? -1 : 1;
+        } else if (a.precio > b.precio) {
+            return orden === "ascendente" ? 1 : -1;
+        } else {
+            return 0;
+        }
+        };
+    
+        return productos.sort(compararPorPrecio);
   }
   
-  // Ejemplo de uso
-  const producto = [
-    { nombre: "Luxury Gems Necklase",  precio: 168.76 },
-    { nombre: "Reflections Necklace",  precio: 620.73 },
-    { nombre: "Timeless Halo Earrings",  precio: 620.73 },
-    { nombre: "Exquisite Earrings",  precio: 327.71 },
-  ];
-  
   console.log("Productos ordenados ascendentemente por precio:");
-  console.log(ordenarProductosPorPrecio(productos, "ascendente"));
+  console.log(ordenarProductosPorPrecio(products, "ascendente"));
   
   console.log("\nProductos ordenados descendentemente por precio:");
-  console.log(ordenarProductosPorPrecio(productos, "descendente"));
+  console.log(ordenarProductosPorPrecio(products, "descendente"));
