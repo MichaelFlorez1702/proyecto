12c9bd7eed5d53c787e68b0918ad4bb3db9b8c76
jhonfredybetancur <fredybencar22@gmail.com>
2024-07-03 15:14:43 -0500

    jhon

--- scripts/scrip3.js
+++ scripts/scrip3.js
@@ -167,3 +167,56 @@ const products = [
     producto1_line4,
     producto2_line4
 ];
+//Ejercicio 2
+// Función para filtrar productos por tipo
+function filtrarProductosPorTipo(productos, tipoProducto) {
+    const productosFiltrados = productos.filter(producto => producto.tipo === tipoProducto);
+    return productosFiltrados;
+  }
+  
+  // Lista de productos
+  const productos = [
+    { nombre: "Luxury Gems Necklase", tipo: "Accesorio", precio: 168.76 },
+    { nombre: "Reflections Necklace", tipo: "Accesorio", precio: 620.73 },
+    { nombre: "Timeless Halo Earrings", tipo: "Accesorio", precio: 620.73 },
+    { nombre: "Exquisite Earrings", tipo: "Accesorio", precio: 327.71 },
+    
+  ];
+  
+  // Filtrar productos por tipo de accesorio
+  const accesoriosFiltrados = filtrarProductosPorTipo(productos, "Accesorio");
+  
+  // Mostrar resultado en la consola del navegador
+  console.log("Accesorios disponibles:");
+  console.table(accesoriosFiltrados); 
+  
+
+  // Ejercicio 4
+// Función para ordenar un array de productos por precio (ascendente o descendente)
+function ordenarProductosPorPrecio(productos, orden) {
+    const compararPorPrecio = (a, b) => {
+      if (a.precio < b.precio) {
+        return orden === "ascendente" ? -1 : 1;
+      } else if (a.precio > b.precio) {
+        return orden === "ascendente" ? 1 : -1;
+      } else {
+        return 0;
+      }
+    };
+  
+    return productos.sort(compararPorPrecio);
+  }
+  
+  // Ejemplo de uso
+  const producto = [
+    { nombre: "Luxury Gems Necklase",  precio: 168.76 },
+    { nombre: "Reflections Necklace",  precio: 620.73 },
+    { nombre: "Timeless Halo Earrings",  precio: 620.73 },
+    { nombre: "Exquisite Earrings",  precio: 327.71 },
+  ];
+  
+  console.log("Productos ordenados ascendentemente por precio:");
+  console.log(ordenarProductosPorPrecio(productos, "ascendente"));
+  
+  console.log("\nProductos ordenados descendentemente por precio:");
+  console.log(ordenarProductosPorPrecio(productos, "descendente"));
